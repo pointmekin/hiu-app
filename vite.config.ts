20897d487ebf442f0fc614ea02829b93b897f06c
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2026-05-19 23:33:16 +0700

    fix: vercel configs

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,16 @@ import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
 	resolve: { tsconfigPaths: true },
-	plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+	plugins: [
+		devtools(),
+		tailwindcss(),
+		tanstackStart(),
+		nitro({ preset: "vercel" }),
+		viteReact(),
+	],
 });
